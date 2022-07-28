25f2f524a1da679215090bf7d0c3801a62e03bd2
Hathaway-Shan <hathaway.shan@gmail.com>
2022-07-28 10:15:20 -0700

    wrote first test for returning all items in table

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,4 +3,15 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+describe('magic routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  })
+});
 
+//test for return of full list of DB items
+
+it('/magic returns a list of magic cards', async () => {
+  const res = await request(app).get('/magic');
+  expect(res.body.length).toEqual(3);
+});
